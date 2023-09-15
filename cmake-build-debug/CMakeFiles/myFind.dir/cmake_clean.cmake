eedbc4856f132c00be5128962464bc1393eb00aa
Magnus <magnus_goeppel@protonmail.com>
2023-09-15 21:21:33 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/myFind.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myFind.dir/main.cpp.o"
+  "CMakeFiles/myFind.dir/main.cpp.o.d"
+  "myFind"
+  "myFind.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myFind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/myFind.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for myFind.
+# This may be replaced when dependencies are built.
